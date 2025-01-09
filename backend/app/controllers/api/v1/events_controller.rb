1f2d26df2f17ffb632aa99a765f60aef351c419b
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-01-08 17:46:20 -1000

    added Devise and User Model

--- backend/app/controllers/api/v1/events_controller.rb
+++ backend/app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::EventsController < ApplicationController
-  
+  before_action :set_event, only: %i[show update destroy]
+
   # GET api/v1/events
   def index
     # show all events
@@ -10,35 +11,56 @@ class Api::V1::EventsController < ApplicationController
   # POST api/v1/events
   def create
     # create a new event
-    event = Event.new(event_params)
+    @event = Event.new(event_params)
 
-    if event.save
-      render json: {message: "event was added successfully", data: event}
+    if @event.save
+      render json: {message: "event was added successfully", data: @event}
     else
-      render json: {message: "failed to add event", data: event.errors}
+      render json: {message: "failed to add event", data: @event.errors}
     end
   end
 
   # SHOW api/v1/events/:id
   def show
     # show a single event
+    if @event
+      render json: {message: "event found", data: @event}
+    else
+      render json: {message: "event not found", data: @event.errors }
+    end
   end
 
   # PATCH/PUT api/v1/events/:id
   def update
-    # update a single event
+    # update a single event 
+    if @event.update(event_params)
+      render json: {message: "event updated", data: @event}
+    else
+      render json: {message: "event not updated", data: @event.errors }
+    end
   end
 
   # DELETE api/v1/events/:id
   def destroy
     # delete a single event
+    if @event.destroy
+      render json: {message: "event deleted", data: @event}
+    else
+      render json: {message: "unable to delete the event", data: @event.errors }
+    end
   end
 
-
-end
-
-private
+  private
 
 def event_params
   params.permit(:title, :description, :start_time, :end_time, :location)
+  # params.require(:event)permit(:title, :description, :start_time, :end_time, :location)
 end
+
+def set_event
+  @event = Event.find(params[:id])
+end
+
+end
+
+
