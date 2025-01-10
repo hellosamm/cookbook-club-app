2e62c7cc7b0382101493d7c309676ebcb32ae8ed
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-01-09 17:24:58 -1000

    Registration Delete request is successfully working

--- backend/app/controllers/concerns/rack_session_fix.rb
+++ backend/app/controllers/concerns/rack_session_fix.rb
@@ -1,15 +1,22 @@
 module RackSessionFix
   extend ActiveSupport::Concern
+  
   class FakeRackSession < Hash
     def enabled?
       false
     end
   end
+  
   included do
-    before_action :set_fake_rack_session_for_devise
+    before_action :set_fake_rack_session_for_devise, only: [:destroy]
+  end
     private
+
     def set_fake_rack_session_for_devise
       request.env['rack.session'] ||= FakeRackSession.new
+
+      if request.method == "DELETE"
+        request.env['rack.session'] ||= FakeRackSession.new
+      end
     end
-  end
 end
--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  include RackSessionFix
   respond_to :json
+
+  private
+  
+  def respond_with(resource, _opts = {})
+    if request.method == "POST" && resource.persisted?
+      render json: { message: "Logged in sucessfully.", data: resource}, status: :ok
+    elsif request.method == "DELETE"
+      render json: { message: "Logged out successfully." }, status: :ok 
+    else
+      render json: { 
+        message: "Couldn't find an actice session.", 
+        # errors: resource.errors.full_messages.to_sentence 
+      }, status: :unauthorized 
+    end
+  end
 end
