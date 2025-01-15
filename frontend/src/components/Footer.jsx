b979f6566d236d6bea23c930efb63b33a91f3a1d
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-01-15 13:21:27 -1000

    footer is updated with logos & links

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import instagram from "/public/instagram.png";
+import github from "/public/github.png";
+import linkedin from "/public/linkedin.png";
 
 const Footer = () => {
   return (
-    <div>
-      <p>testing the footer</p>
+    <div className="flex flex-col items-center">
+      <div className="flex space-x-4 items-center">
+        <a href="https://www.instagram.com/samm.bakerr/" target="blank">
+          <img src={instagram} className="w-6 " />
+        </a>
+        <a href="https://github.com/hellosamm" target="blank">
+          <img src={github} className="w-6" />
+        </a>
+        <a href="https://www.linkedin.com/in/samm-bakerr/" target="blank">
+          <img src={linkedin} className="w-6" />
+        </a>
+      </div>
+      <div className="flex items-center ">
+        <div className="pr-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+            />
+          </svg>
+        </div>
+        <p>made by samm 2025</p>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 bg-ivory">
-      <div className="flex">
-        <div>Logo</div>
-        <div>
-          <button>Login</button>
+    <div className=" bg-ivory">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center py-3">
+          <div>cookbook club</div>
+          <div className="">
+            <button className="bg-ivory text-black rounded-sm py-1 px-2 hover:bg-black hover:text-white">
+              login
+            </button>
+          </div>
         </div>
       </div>
     </div>
