0d77a1fc860bc35252e305f58e721632b9406634
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-01-21 10:17:04 -1000

    updated CORS

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  # before_action :handle_options_request
+
+  # def handle_options_request
+  #   if request.method == "OPTIONS"
+  #     if request.method == "OPTIONS"
+  #       headers["Access-Control-Allow-Origin"] = "*"
+  #       headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+  #       headers["Access-Control-Allow-Headers"] = "*"
+  #       render plain: "", content_type: "text/plain"
+  #     end
+  #   end
+  # end
 end
