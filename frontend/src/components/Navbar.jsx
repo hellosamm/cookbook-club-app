a3c7908b4db67a7514cf6d00b2007cf89032b9be
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-01-29 12:23:05 -1000

    updated navbar UI to include a dropdown for users to access profile & logout buttons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import { logoutApi } from "../apis/authentication";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 const Navbar = () => {
   const [cookies, setCookies, removeCookie] = useCookies([]);
-  const [signinState, setSigninState] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (cookies.authToken) {
       // set state to logged in
-      setSigninState(true);
+      setLoggedIn(true);
       console.log("sign in state:", cookies.authToken);
     }
   }, [cookies.authToken]);
@@ -34,7 +35,7 @@ const Navbar = () => {
 
     if (result && !error) {
       removeCookie("authToken");
-      setSigninState(false);
+      setLoggedIn(false);
       navigate("/");
     }
   };
@@ -42,22 +43,46 @@ const Navbar = () => {
   return (
     <div className=" bg-ivory">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-3">
+        <div className="flex justify-between py-3">
           <Link to="/">cookbook club</Link>
-          <Link to="profile">profile</Link>
-          {signinState === true ? (
-            <div className="">
-              <Link
-                to="/"
-                className="bg-ivory text-black rounded-sm py-1 px-2 hover:bg-black hover:text-white"
-                onClick={async (e) => {
-                  e.preventDefault();
-                  await handleLogout();
-                }}
-              >
-                logout
-              </Link>
-            </div>
+
+          {loggedIn ? (
+            <Menu as="div" className="">
+              <MenuButton className="flex items-center rounded-sm py-1 px-2 ">
+                @username
+                <img
+                  src="/public/down-arrow.png"
+                  alt="Dropdown Icon"
+                  className="ml-2 w-6 h-6"
+                />
+              </MenuButton>
+              <MenuItems className="transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
+                <div>
+                  <MenuItem>
+                    <Link
+                      className="block  bg-ivory text-black rounded-sm py-1 px-2 hover:bg-black hover:text-white  "
+                      to="profile"
+                    >
+                      profile
+                    </Link>
+                  </MenuItem>
+                  <MenuItem>
+                    <Link
+                      to="/"
+                      className=" block  bg-ivory text-black rounded-sm py-1 px-2 hover:bg-black hover:text-white "
+                      role="menuitem"
+                      tabIndex={-1}
+                      onClick={async (e) => {
+                        e.preventDefault();
+                        await handleLogout();
+                      }}
+                    >
+                      logout
+                    </Link>
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </Menu>
           ) : (
             <div className="">
               <Link
