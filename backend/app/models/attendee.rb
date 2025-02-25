337809063bf613692b85cc0875d41eb9328fe874
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-02-24 16:57:41 -1000

    db updated to have 2 users and seeded with 3 events

--- backend/app/models/attendee.rb
+++ backend/app/models/attendee.rb
@@ -1,4 +1,6 @@
 class Attendee < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :user_id, uniqueness: { scope: :event_id, message: "has already signed up for this event" }
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,9 +9,27 @@
 #   end
 
 
-Event.create(title: "January Cookbook Club", 
-description: "Bring your favorite nourishing food to share. You can also bring a topping for buddah bowls.", 
-start_time: DateTime.now + 2.days + 2.hours,
-end_time: DateTime.now + 2.days + 5.hours,
-location: "66-000 Kam Highway Haleiwa")
+Event.create!([
+  {title: "Sewing Night", 
+  description: "Bring your sewing machine and a project to work on!", 
+  start_time: DateTime.new(2025, 3, 14, 16, 0),
+  end_time: DateTime.new(2025, 3, 14, 19, 30),
+  location: "Haleiwa",
+  user_id: 1}, 
+  {title: "Tech Networking", 
+  description: "Come meet other women in tech!", 
+  start_time: DateTime.new(2025, 3, 18, 15, 30),
+  end_time: DateTime.new(2025, 3, 18, 17, 0),
+  location: "Virtual",
+  user_id: 1}, 
+  {title: "March Cookbook Club", 
+  description: "Bring your favorite nourishing food to share. You can also bring a topping for buddha bowls.", 
+  start_time: DateTime.new(2025, 3, 27, 17, 30),
+  end_time: DateTime.new(2025, 3, 27, 20, 30),
+  location: "Haleiwa Beach Park",
+  user_id: 2}
+])
+
+
+
 
