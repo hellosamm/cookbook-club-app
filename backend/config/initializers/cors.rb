e94d9c7f3602550fc35b7a7ccf221c65fd1a63b5
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-03-27 15:24:38 -1000

    updated cors origins url

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -18,7 +18,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins 'https://cookbook-club-rails-api.onrender.com'
     resource(
       '*',
       headers: :any,
